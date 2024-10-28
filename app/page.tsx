bdaf35bfb4ffd7152a4552e9372c18ee63a10d52
humbl3LilaS <sabishinekobebe@gmail.com>
2024-10-28 21:13:39 +0630

    add: Property listing in home page with button to navigate to /properties

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Info from "@/components/Info";
+import HomeProperty from "@/components/HomeProperty";
 
 export default function Home() {
     return (
         <>
             <Hero/>
             <Info/>
+            <HomeProperty/>
         </>
     );
 }
---
+++ components/HomeProperty.tsx
@@ -0,0 +1,32 @@
+import properties from "@/properties.json"
+import PropertyCard, {PropertyData} from "@/components/PropertyCard";
+import Link from "next/link";
+
+const HomeProperty = () => {
+
+    const recentProperties = properties.sort(() => Math.random() - Math.random()).slice(0,
+                                                                                        3) as unknown as PropertyData[];
+
+    return (
+        <section className="px-4 py-6">
+            <div className="container-xl lg:container m-auto">
+                <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
+                    Recent Properties
+                </h2>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                    {recentProperties.length === 0 && <p>No Properties </p>}
+                    {recentProperties.length > 0 && recentProperties.map(
+                        (item, index) => (<PropertyCard key={index} data={item}/>))}
+                </div>
+            </div>
+            <div className={"py-5 md:py-8"}>
+                <Link href={"/properties"}
+                      className={"block mx-auto w-fit py-4 px-6 rounded-xl bg-black text-white uppercase tracking-wide font-semibold transition-colors duration-500 hover:bg-gray-700"}>
+                    View all properties
+                </Link>
+            </div>
+        </section>
+    );
+};
+
+export default HomeProperty;
